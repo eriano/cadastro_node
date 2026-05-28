a9a9ca0493236259312148d1c95d664a623975a1
eriano <31052277+eriano@users.noreply.github.com>
2026-05-28 15:25:22 -0300

    Update api.js
    troca url DB

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: 'https://node-zero-8uqg.onrender.com'
+    baseURL: 'https://node-zero-olqg.onrender.com'
     //baseURL: 'http://localhost:3333'
 })
 
